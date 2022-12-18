921954bce1ddf16daa2751a45f0ac15f6c278c76
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-18 16:55:55 +0300

    Interface completed.

---
+++ ObjectOrientedPrograming/Interface/Example2/Cikar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograming.Interface.Example2
+{
+    internal interface Cikar
+    {
+    }
+}
